3402528195171874907079f13bd849874693ad15
AleAlvs <aleson.a.s@gmail.com>
2017-11-18 15:16:44 -0200

    console

--- controllers/top.js
+++ controllers/top.js
@@ -12,26 +12,27 @@ module.exports = function (app) {
             getCount();
 
             function getCount() {
-                deviceModel.find().count(function(error, count){
+                deviceModel.find().count(function (error, count) {
                     getTopRated(count);
                 });
             }
 
             function getTopRated(count) {
                 apodModel.find({}).limit(10).sort({ averageRate: -1 }).select({
-                        date: 1,
-                        explanation: 1,
-                        hdurl: 1,
-                        media_type: 1,
-                        service_version: 1,
-                        title: 1,
-                        url: 1,
-                        averageRate: 1,
-                        votes: 1
-                    }).exec(function (error, callback) {
+                    date: 1,
+                    explanation: 1,
+                    hdurl: 1,
+                    media_type: 1,
+                    service_version: 1,
+                    title: 1,
+                    url: 1,
+                    averageRate: 1,
+                    votes: 1
+                }).exec(function (error, callback) {
+                    if (dev)
                         console.log("Lista de melhoores avaliacoes");
-                        res.send(callback);
-                    });
+                    res.send(callback);
+                });
             }
         },
     };
