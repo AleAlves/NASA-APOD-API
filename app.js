ad2a4313a74826cac9c628228e821cbf1fdb2861
Aleson Alves de Souza <aleson.a.s@gmail.com>
2018-01-09 10:44:36 -0200

    testes xablau

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var express = require('express'),
   admin = require("firebase-admin");
 
 global.dev = false;
-global.version = "4.0.0";
+global.version = "4.0.1";
 
 var config = {
   apiKey: "AIzaSyA9ci6gRvrszwCafUjEtpaKdfJtkyQy_5Q",
